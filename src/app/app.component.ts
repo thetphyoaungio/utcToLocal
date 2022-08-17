1435b7c332fac8bcfc64ee4cb35adecc5ce88b76
thetphyoaungio <kyawaung0003@gmail.com>
2022-08-17 15:39:06 +0630

    init files add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'utcToLocal';
+
+  localDateTimeResult:string|any;
+
+  constructor(private datePipe:DatePipe){this.localDateTimeResult = 'Converted Local Time here...';}
+
+  onConvert(){
+    const utcInputEl = <any>document.querySelector('#utcInput');
+    if(utcInputEl){
+      console.log(utcInputEl.value);
+
+      if(utcInputEl.value){
+        const t = new Date(utcInputEl.value);
+        console.log(t);
+        
+        t.setMinutes(t.getMinutes()+30);
+        t.setHours(t.getHours()+6);
+        console.log('After/t',t);
+
+        this.localDateTimeResult = this.datePipe.transform(t, 'yyyy-MM-dd hh:mm:ss a');
+      }
+    }
+  }
+
+  onReset(){
+    const utcInputEl = <any>document.querySelector('#utcInput');
+    
+    if(utcInputEl){
+      if(utcInputEl.value) utcInputEl.value=null;
+    }
+    
+    this.localDateTimeResult = 'Converted Local Time here...';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DatePipe } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
